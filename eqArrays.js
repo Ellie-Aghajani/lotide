abefe6bdf17a66f979bf9b8a300fa2cf429273cd
Ellie-Aghajani <z.aqajani@gmail.com>
2022-11-17 00:51:38 -0800

    added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+
+  const eqArrays = function(array1, array2) {
+    for( let i = 0; i < array1.length; i++){
+        if(array1[i]=== array2[i]){
+            return true;
+
+        }else {
+            return false;
+        }
+    }
+  }
+  
+  // TEST CODE
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
