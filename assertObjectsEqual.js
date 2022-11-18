4ed92c77b7a72c516077a9a8d3f2683a668e7cb6
Ellie-Aghajani <z.aqajani@gmail.com>
2022-11-18 00:58:46 -0800

    added function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,16 @@
+const eqObjects = require("./eqObjects");
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect; // <= add this line
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+module.exports = assertObjectsEqual;
