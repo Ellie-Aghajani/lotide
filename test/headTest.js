3121066e72e5e615dccee8d5623e68b4438a7a7f
Ellie-Aghajani <z.aqajani@gmail.com>
2022-11-22 15:28:51 -0800

    added chai and mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
+
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
